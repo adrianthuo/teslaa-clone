307f1875b7f9f651413ca39e0a587ea807712a8e
adrianthuo <adrianthuo@gmail.com>
2023-12-26 22:49:03 +0300

    initial commit

---
+++ teslaa-clone/src/Components/Section.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section() {
+  return (
+    <Wrap>
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Orde Online for Touchles Delivery</p>
+            </ItemText>
+         <Buttons>  
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg">
+
+            </DownArrow>
+            </Buttons>
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+
+    width: 100vw;
+    height:100vh;
+    background-position: center;
+    background-repeat: norepeat;
+    background-image: url(/images/model-s.jpg);
+    display : flex;
+    flex-direction : column;
+    justify-content: space-between;
+    align-items: center;
+
+`
+const ItemText = styled.div`
+
+    padding-top: 15vh;
+    text-align:center;
+    margin-bottom: 30px;
+
+
+`
+
+const ButtonGroup = styled.div`
+     
+    display: flex;
+
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23 ,26, 32, 0.8);
+    height: 40px;
+    width:256px;
+    color: white;
+    display : flex;
+    justify-content: center;
+    align-items:center;
+    border-radius: 100px;
+    opacity:0.85;
+    text-transform:uppercase;
+    font-size : 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+
+`
+
+const DownArrow = styled.img`
+
+    margin-top: 20px;
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+
+`
+
+const Buttons = styled.div`
+
+
+
+`
