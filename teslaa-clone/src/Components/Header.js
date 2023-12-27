8e376c190ee766c57d36a3e0c162a03c266b7daa
adrianthuo <adrianthuo@gmail.com>
2023-12-27 22:44:48 +0300

    header section

--- teslaa-clone/src/Components/Header.js
+++ teslaa-clone/src/Components/Header.js
@@ -1,11 +1,85 @@
-import React from 'react'
+  import React from 'react'
+  import styled from 'styled-components'
+  import MenuIcon from '@mui/icons-material/Menu';
+  function Header() {
+    return (
+      <Container>
+        <a>
+          <img src='/images/logo.svg' alt='logo'/>
+        </a>
 
-function Header() {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+        <Menu>
+          <a href='#'>Model s</a>
+          <a href='#'>Model 3</a>
+          <a href='#'>Model x</a>
+          <a href='#'>Model y</a>
+          <a href='#'>Cybertruck</a>
+        </Menu>
+        <RightMenu>
+          <a href='#'>Shop</a>
+          <a href='#'>Tesla Account</a>
+        </RightMenu>
 
-export default Header
+        <CustomMenu>
+
+        </CustomMenu>
+        
+      </Container>
+    )
+  }
+
+  export default Header
+
+
+  const Container = styled.div`
+
+    min-height:60px;
+    position:fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding : 0 20px;
+    top:0;
+    left: 0;
+    right: 0;
+
+  `
+  const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    a{
+      font=weight: 600px;
+      text-transform: uppercase;
+      padding : 0 10px;
+      text-wrap: nowrap;
+    }
+
+    @media(max-width: 768px){
+      display: none;
+    }
+
+
+  `
+
+  const RightMenu = styled.div`
+
+      display: flex;
+      align-items: center;
+
+  a{
+    font=weight: 600px;
+    text-transform: uppercase;
+    margin-right : 10px;
+    
+  }
+
+  `
+
+  const CustomMenu = styled(MenuIcon)`
+
+    cursor: pointer;
+
+  `
--- teslaa-clone/src/Components/Home.js
+++ teslaa-clone/src/Components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from "styled-components"
 import Section from './Section'
+import Header from './Header'
 function Home() {
   return (
     <Container>
+      <Header/>
             <Section
             title="Model 3"
               description="Order Online for Touchles Deliveries"
@@ -21,7 +23,7 @@ function Home() {
             />
             <Section
             
-            title="Model s"
+            title="Model S"
               description="Order Online for Touchles Deliveries"
               backgroundImg="model-s.jpg"
               leftBtnText="Custom order"
